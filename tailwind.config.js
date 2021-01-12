9adc150c3e9cd00202ea26c51ff4391acafba063
ddgoodscoede <anonymous>
2021-01-12 16:49:42 +0300

    Customize Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
     purge: {
         content: [
@@ -8,10 +10,26 @@ module.exports = {
         ]
     },
     theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            white: colors.white,
+            gray: colors.trueGray,
+            green: colors.green,
+            teal: colors.teal,
+            red: colors.red,
+            yellow: colors.yellow,
+            blue: colors.blue,
+        },
         extend: {},
     },
     variants: {
-        extend: {},
+        extend: {
+            backgroundColor: ['active'],
+            borderWidth: ['active'],
+            borderColor: ['active', 'focus'],
+        },
     },
     plugins: [
         require('@tailwindcss/forms'),
