4c28f244f8dc566b2cda9ad53aeb6fe58bf7cc0e
ddgoodscoede <anonymous>
2021-01-14 18:38:09 +0300

    Update config.php

--- config.php
+++ config.php
@@ -6,4 +6,9 @@ return [
     'title' => 'Jigsaw',
     'description' => 'Website description.',
     'collections' => [],
+    'assetsUrl' => 'assets',
+    'imagesUrl' => 'assets/images',
+    'onionAddress' => 'ddgoodscoede23n56nlrblsudrlnhgyjwtq5cbpqn4zv24ff7hwvakqd.onion',
+    'onionUrl' => 'http://ddgoodscoede23n56nlrblsudrlnhgyjwtq5cbpqn4zv24ff7hwvakqd.onion/',
+    'clearnetUrl' => 'https://ddru.cc/',
 ];
