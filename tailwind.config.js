7365ec2004096ee8532c9e80475149764e8f0649
ddgoodscoede <anonymous>
2021-01-19 11:32:35 +0300

    Add Tailwind line-clamp plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -80,6 +80,7 @@ module.exports = {
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
         require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/line-clamp'),
         require('tailwindcss-debug-screens'),
     ],
 }
