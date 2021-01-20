a37db85bfb934e86496ffd915f0416f025ba4f45
ddgoodscoede <anonymous>
2021-01-20 12:33:19 +0300

    Create code block component

---
+++ source/_components/code.blade.php
@@ -0,0 +1,5 @@
+<pre
+    {{ $attributes->merge([
+        'class' => 'overflow-x-auto bg-gray-800 text-gray-200 rounded-md px-5 py-3.5 text-sm leading-relaxed',
+    ]) }}
+><code>{{ $slot }}</code></pre>
