ea4339ce5b414160e7b4a2dbcffcc487c4e46947
ddgoodscoede <anonymous>
2021-01-27 14:56:45 +0300

    Improved responsive logos

--- source/_components/navbar.blade.php
+++ source/_components/navbar.blade.php
@@ -2,18 +2,18 @@
     <div class="container h-full mx-auto flex flex-nowrap items-stretch">
 
         {{-- Brand --}}
-        <div class="flex-none flex items-center">
-            <a class="py-2 px-3" href="{{ url('/') }}">
+        <div class="flex-none flex items-stretch">
+            <a class="py-2 px-3 w-20 lg:w-36" href="{{ url('/') }}">
+                {{-- Smaller logo on mobile --}}
                 <img
-                    class="hidden lg:block"
-                    src="{{ $page->image('dd-info-2x.png') }}"
-                    width="140"
+                    class="lg:hidden object-contain object-center h-full w-full"
+                    src="{{ $page->image('logo-small-2x.png') }}"
                     alt="Логотип dd Marketplace"
                 >
+                {{-- Bigger logo on desktop --}}
                 <img
-                    class="lg:hidden"
-                    src="{{ $page->image('logo-small-2x.png') }}"
-                    width="80"
+                    class="hidden lg:block object-contain object-center h-full w-full"
+                    src="{{ $page->image('dd-info-2x.png') }}"
                     alt="Логотип dd Marketplace"
                 >
             </a>
