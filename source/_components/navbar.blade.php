bca591aa82123d95fd6d28f9308f51a8145d7265
ddgoodscoede <anonymous>
2021-01-27 14:42:59 +0300

    Make navbar responsive

--- source/_components/navbar.blade.php
+++ source/_components/navbar.blade.php
@@ -21,35 +21,42 @@
 
         {{-- Start --}}
         <div class="flex flex-nowrap items-stretch mr-auto">
+
             <x-navbar.link href="{{ url('/') }}">
                 <span>Как зайти на dd</span>
             </x-navbar.link>
-            <x-navbar.link href="{{ url('tor-browser') }}">
+            <x-navbar.link class="hidden sm:flex" href="{{ url('tor-browser') }}">
                 <span>Тор Браузер</span>
             </x-navbar.link>
-            <x-navbar.link href="{{ url('mirrors') }}">
+            <x-navbar.link class="hidden md:flex" href="{{ url('mirrors') }}">
                 <span>Зеркала dd</span>
             </x-navbar.link>
-            <x-navbar.link href="{{ url('payments') }}">
+            <x-navbar.link class="hidden lg:flex" href="{{ url('payments') }}">
                 <span>Пополнение баланса</span>
             </x-navbar.link>
-            <x-navbar.link href="{{ url('purchases') }}">
+            <x-navbar.link class="hidden xl:flex" href="{{ url('purchases') }}">
                 <span>Покупки на dd</span>
             </x-navbar.link>
-            <x-navbar.link href="{{ url('news') }}">
+            <x-navbar.link class="hidden xl:flex" href="{{ url('news') }}">
                 <span>Новости</span>
             </x-navbar.link>
 
-            <x-navbar.dropdown>
+            <x-navbar.dropdown class="xl:hidden">
                 <x-slot name="button">Ещё</x-slot>
-                <x-navbar.dropdown-item>
-                    <a href="">Foobar 1!</a>
+                <x-navbar.dropdown-item class="sm:hidden">
+                    <a href="{{ url('tor-browser') }}">Тор Браузер</a>
                 </x-navbar.dropdown-item>
-                <x-navbar.dropdown-item>
-                    <a href="">Foobar 2!</a>
+                <x-navbar.dropdown-item class="md:hidden">
+                    <a href="{{ url('mirrors') }}">Зеркала dd</a>
                 </x-navbar.dropdown-item>
-                <x-navbar.dropdown-item>
-                    <a href="">Foobar 2!</a>
+                <x-navbar.dropdown-item class="lg:hidden">
+                    <a href="{{ url('payments') }}">Пополнение баланса</a>
+                </x-navbar.dropdown-item>
+                <x-navbar.dropdown-item class="xl:hidden">
+                    <a href="{{ url('purchases') }}">Покупки на dd</a>
+                </x-navbar.dropdown-item>
+                <x-navbar.dropdown-item class="xl:hidden">
+                    <a href="{{ url('news') }}">Новости</a>
                 </x-navbar.dropdown-item>
             </x-navbar.dropdown>
 
