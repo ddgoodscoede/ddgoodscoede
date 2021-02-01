bcd9f4072745f0b515b42f858a90ae245392664d
ddgoodscoede <anonymous>
2021-02-01 16:30:07 +0300

    Dropdown made to work on touch

--- source/_components/navbar/dropdown.blade.php
+++ source/_components/navbar/dropdown.blade.php
@@ -1,7 +1,9 @@
 <div
     {{ $attributes->merge([
-        'class' => 'group relative hover:bg-gray-50 hover:text-blue-600 cursor-pointer flex items-center transition duration-100',
+        'class' => 'group relative hover:bg-gray-50 hover:text-blue-600 cursor-pointer flex items-center transition duration-100 select-none',
     ]) }}
+    x-data="{ open: false }"
+    x-on:click.stop="open = !open"
 >
 
     <div class="px-3 py-2 flex items-center">
@@ -10,7 +12,10 @@
     </div>
 
     <ul
-        class="invisible opacity-0 top-full-skewed-4 group-hover:visible group-hover:opacity-100 group-hover:transform group-hover:translate-y-2 transition-all duration-100 min-w-full absolute border rounded-md bg-white py-2"
+{{--        class="invisible opacity-0 top-full-skewed-4 group-hover:visible group-hover:opacity-100 group-hover:transform group-hover:translate-y-2 transition-all duration-100 min-w-full absolute border rounded-md bg-white py-2"--}}
+        class="top-full-skewed-4 min-w-full absolute border rounded-md bg-white py-2 transition-all duration-100 group-hover:visible group-hover:opacity-100 group-hover:transform group-hover:translate-y-2"
+        x-bind:class="{ 'invisible opacity-0': !open, 'transform translate-y-2': open }"
+        @click.away="open = false"
     >
         {{ $slot }}
     </ul>
