80728cf3dbbf6769a82aca9bba2288b109e39e91
ddgoodscoede <anonymous>
2021-02-09 16:03:33 +0300

    Add Yandex Webmaster verification

---
+++ source/yandex_a5def62b39759fd0.blade.php
@@ -0,0 +1,10 @@
+---
+permalink: yandex_a5def62b39759fd0.html
+---
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>Verification: a5def62b39759fd0</body>
+</html>
