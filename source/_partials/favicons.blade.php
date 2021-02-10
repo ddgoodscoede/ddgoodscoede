2f3cb5536807ddf5ffcada5b45a769f513aaa202
ddgoodscoede <anonymous>
2021-02-10 15:20:57 +0300

    Update favicons.blade.php

--- source/_partials/favicons.blade.php
+++ source/_partials/favicons.blade.php
@@ -5,7 +5,7 @@
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 <link rel="manifest" href="/site.webmanifest">
 <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00d1b2">
-<meta name="apple-mobile-web-app-title" content="dd Marketplace">
-<meta name="application-name" content="dd Marketplace">
+<meta name="apple-mobile-web-app-title" content="dd Marketplace Info">
+<meta name="application-name" content="dd Marketplace Info">
 <meta name="msapplication-TileColor" content="#00d1b2">
 <meta name="theme-color" content="#fafafa">
